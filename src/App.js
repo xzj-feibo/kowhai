cdbea87e53a1ddfb6aa4a7baa1d38222dd96d5eb
xuzixuan <xzx527416@gmail.com>
2025-01-09 16:29:52 +0800

    feat:用户首页

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import Login from "./pages/Login";
 import UserAvatar from "./components/UserAvatar";
 import UserList from "./components/UserList";
+import UserHome from "./pages/Home";
 class App extends Component {
     render() {
         return (
             // <Login/>
             // <UserAvatar/>
-            <UserList/>
+            // <UserList/>
+            <UserHome/>
         );
     }
 }
