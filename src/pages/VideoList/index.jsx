2fd3bfd9998415049cb805280c5fb1d28d0f689d
feibo <xzj416527@gmail.com>
2025-01-27 22:07:59 +0800

    feat 增加侧边栏和顶部菜单

--- src/pages/VideoList/index.jsx
+++ src/pages/VideoList/index.jsx
@@ -1,12 +1,58 @@
 import VideoItem from '../../components/video/VideoItem';
 import {useEffect, useRef, useState} from "react";
 import {getVideos, searchByName} from "../../api/video";
-import {Box, Divider, Drawer, InputBase} from "@mui/material";
+import {
+    Box,
+    Divider,
+    Drawer,
+    InputBase,
+    List,
+    ListItemIcon,
+    ListItemText
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home"
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import SubscriptionIcon from "@mui/icons-material/Subscriptions"
+import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
+import HistoryOutlinedIcon from '@mui/icons-material/HistoryOutlined';
+import HistoryIcon from '@mui/icons-material/History';
+import PlaylistPlayOutlinedIcon from '@mui/icons-material/PlaylistPlayOutlined';
+import PlaylistPlayRoundedIcon from '@mui/icons-material/PlaylistPlayRounded';
+import VideoLibraryOutlinedIcon from '@mui/icons-material/VideoLibraryOutlined';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import WorkHistoryOutlinedIcon from '@mui/icons-material/WorkHistoryOutlined';
+import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import {AsideListItem, TopButton, TopListItem} from "./VideoList";
 
 export default function VideoList() {
     let [videos, setVideos] = useState([]);
     let [page, setPage] = useState(0);
     let [pageSize, setPageSize] = useState(0);
+
+    //判断顶部菜单项选中
+    const [topIsSelected, setTopIsSelected] = useState(null);
+    //判断侧边栏选项选中
+    const [asideIsSelected, setAsideIsSelected] = useState(null)
+
+    //顶部菜单
+    const topMenuItems = ['全部', '音乐', '美食', '游戏', '鬼畜', '极限运动', '体育', '旅游', '发现新视频']
+
+    //侧边栏选项菜单
+    const asideMenuItems = [
+        { id: 0, text: '首页', defaultIcon: <HomeOutlinedIcon />, selectedIcon: <HomeIcon/> },
+        { id: 1, text: '订阅', defaultIcon: <SubscriptionsOutlinedIcon />, selectedIcon: <SubscriptionIcon/> },
+        { id: 2, text: '点赞过的视频', defaultIcon: <ThumbUpAltOutlinedIcon />, selectedIcon: <ThumbUpAltIcon/> },
+        { id: 3, text: '历史记录', defaultIcon: <HistoryIcon />, selectedIcon: <HistoryOutlinedIcon/> },
+        { id: 4, text: '播放列表', defaultIcon: <PlaylistPlayOutlinedIcon />, selectedIcon: <PlaylistPlayRoundedIcon/> },
+        { id: 5, text: '我的视频', defaultIcon: <VideoLibraryOutlinedIcon />, selectedIcon: <VideoLibraryIcon/> },
+        { id: 6, text: '稍后观看', defaultIcon: <WorkHistoryOutlinedIcon />, selectedIcon: <WorkHistoryIcon/> },
+    ]
+
+    //侧边栏订阅栏目
+    const subscriptions = [{}]
     const inputRef = useRef("");
 
     useEffect(() => {
@@ -35,6 +81,11 @@ export default function VideoList() {
         //todo
     }
 
+    //处理侧边栏选项被点击事件
+    function handleListItemClick(id) {
+        setAsideIsSelected(id);
+    }
+
     return (
         <Box sx={{ display: 'flex', backgroundColor: 'black' }}>
             {/* 侧边栏 */}
@@ -52,14 +103,55 @@ export default function VideoList() {
                 }}
             >
                 <Box sx={{ padding: 2 }}>
-                    <Box sx={{ display: 'inline-flex', alignItems: 'center' }}>
+                    <Box sx={{display: 'inline-flex', alignItems: 'center'}}>
+                        <img src='/imgs/kowhai.png' style={{width: '50px', height: '40px', marginLeft: '8px'}}
+                             alt='无图片'/>&nbsp;&nbsp;
                         <h3>Kowhai</h3>
-                        <img src='/imgs/kowhai.png' style={{ width: '50px', height: '40px', marginLeft: '8px' }} />
                     </Box>
-                    <Divider />
-                    <p>Home</p>
-                    <p>My Videos</p>
-                    <p>Favorites</p>
+                    <List sx={{ width: '200px' }}>
+                        {asideMenuItems.map((item) => {
+                            // 如果 item.id === 3，则先插入 Divider
+                            if (item.id === 3) {
+                                return (
+                                    <>
+                                        <Divider key={`divider-${item.id}`} sx={{ backgroundColor: 'white', marginY: '5px' }} />
+                                        <AsideListItem button>
+                                            <Box sx={{marginRight: '7px'}}>我</Box>
+                                            <ListItemIcon sx={{ color: 'white', minWidth: '30px' }}>
+                                                <ArrowForwardIosIcon sx={{fontSize: '16px'}}/>
+                                            </ListItemIcon>
+                                        </AsideListItem>
+                                        <AsideListItem
+                                            key={item.id}
+                                            button
+                                            onClick={() => handleListItemClick(item.id)}
+                                            isSelected={item.id === asideIsSelected}
+                                        >
+                                            <ListItemIcon sx={{ color: 'white', minWidth: '40px' }}>
+                                                {item.id === asideIsSelected ? item.selectedIcon : item.defaultIcon}
+                                            </ListItemIcon>
+                                            <ListItemText primary={item.text} />
+                                        </AsideListItem>
+                                    </>
+                                );
+                            }
+
+                            // 正常渲染菜单项
+                            return (
+                                <AsideListItem
+                                    key={item.id}
+                                    button
+                                    onClick={() => handleListItemClick(item.id)}
+                                    isSelected={item.id === asideIsSelected}
+                                >
+                                    <ListItemIcon sx={{ color: 'white', minWidth: '40px' }}>
+                                        {item.id === asideIsSelected ? item.selectedIcon : item.defaultIcon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={item.text} />
+                                </AsideListItem>
+                            );
+                        })}
+                    </List>
                 </Box>
             </Drawer>
 
@@ -80,15 +172,26 @@ export default function VideoList() {
                     marginBottom: '15px'
                 }}>
                     <Box sx={{
-                        border: '3px, solid, #787878',
+                        border: '1px, solid, #787878',
                         borderRadius: '60px',
                         paddingLeft: '15px',
                         width: '600px',
                     }}>
-                        <InputBase sx={{color: 'white'}} placeholder='Search Videos'>
+                        <InputBase sx={{color: 'white', width: '100%'}} placeholder='搜索'>
                             <Divider orientation='vertical' flexItem/>
                         </InputBase>
                     </Box>
+                </Box>
+
+                {/*顶部菜单列表*/}
+                <Box>
+                    {topMenuItems.map((item, index) => (
+                        <TopButton
+                            isSelected={topIsSelected === index}
+                            onClick={() => setTopIsSelected(index)}>
+                            {item}
+                        </TopButton>
+                    ))}
                 </Box>
 
                 {/* 视频列表 */}
