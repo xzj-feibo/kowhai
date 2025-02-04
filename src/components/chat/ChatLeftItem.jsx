6958b92b7536da0fcd74b3aa4be48e14b0281729
feibo <xzj416527@gmail.com>
2025-02-04 15:41:57 +0800

    fix 优化聊天系统时间问题

--- src/components/chat/ChatLeftItem.jsx
+++ src/components/chat/ChatLeftItem.jsx
@@ -1,41 +1,9 @@
 import {Avatar, Box, Typography} from "@mui/material";
 import React from "react";
 import theme from "../../theme";
+import {formatDateTime} from "../../api/chat";
 
 export default function ChatLeftItem({avatar, message, time}) {
-    function formatTime(isoTime) {
-        // 截取时间字符串，确保 Date 对象能正确解析
-        const fixedIsoTime = isoTime.replace(/\.\d+Z$/, match => match.slice(0, 4) + 'Z');
-        const date = new Date(fixedIsoTime);
-        const now = new Date();
-
-        if (isNaN(date.getTime())) {
-            throw new Error("Invalid Date Format");
-        }
-
-        const isToday = date.toDateString() === now.toDateString();
-
-        let hours = date.getHours();
-        const minutes = date.getMinutes().toString().padStart(2, '0');
-        const period = hours >= 12 ? 'PM' : 'AM';
-        hours = hours % 12 || 12;
-
-        if (isToday) {
-            return `${hours}:${minutes} ${period}`;
-        } else {
-            const day = date.getDate();
-            const month = date.toLocaleString('default', { month: 'short' });
-            return `${hours}:${minutes} ${period} ${day}, ${month}`;
-        }
-    }
-
-    const time1 = "2025-02-03T23:19:56.12278Z";
-    console.log(formatTime(time1));
-
-    const time2 = "2025-01-01T09:15:20.000Z";
-    console.log(formatTime(time2));
-
-
     return (
         <Box
             sx={{
@@ -70,7 +38,7 @@ export default function ChatLeftItem({avatar, message, time}) {
                     {message}
                 </Typography>
                 <Typography variant="caption" sx={{ display: 'block', textAlign: 'right', fontSize: '10px', marginTop: '4px' }}>
-                    {formatTime(time)}
+                    {formatDateTime(time)}
                 </Typography>
             </Box>
         </Box>
